731db51e12b19d002694e114133f86680672500a
karan raj pareek <karanrajpareek@gmail.com>
2024-10-11 15:20:55 +0530

    vercel changes

--- api/data.js
+++ api/data.js
@@ -4,11 +4,10 @@ const dataRoute = require('../routes/dataroutes');
 
 const app = express();
 
-// Middleware
+
 app.use(express.json());
 app.use("/", dataRoute);
 
-// Export it as a function to use it as a serverless function
 module.exports = (req, res) => {
   app(req, res);
 };
